4d9e9e3397eba58bed01910ebed550463becf51c
AKALYA S <akalyajsa9976@gmail.com>
2026-09-13 12:55:30 +0530

    Time: 6 ms (16.74%) | Memory: 48.9 MB (13.62%) - LeetSync

---
+++ 338-counting-bits/counting-bits.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] arr=new int[n+1];
+        for(int i=0;i<=n;i++){
+        int count=0;
+        int curr=i;
+        while(curr!=0){
+            if((curr&1)==1){
+                count++;
+            }
+            curr=curr>>1;
+            arr[i]=count;
+        }
+        }
+        return arr;
+    }
+}
