d134d7f319d486f3cf7c1e41b98cef2df83c291f
AKALYA S <akalyajsa9976@gmail.com>
2026-09-19 23:28:29 +0530

    Time: 2 ms (100.00%) | Memory: 93.6 MB (24.25%) - LeetSync

--- 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.java
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.java
@@ -3,14 +3,15 @@ class Solution {
         int n=arr.length;
         int a=queries.length;
         int[] ans=new int[a];
+        int[] prefixsum=new int[n+1];
+        for(int i=0;i<n;i++){
+            prefixsum[i+1]=prefixsum[i]^arr[i];
+        }
         for(int i=0;i<a;i++){
             int xor=0;
             int left=queries[i][0];
             int right=queries[i][1];
-            for(int j=left;j<=right;j++){
-                xor^=arr[j];
-            }
-            ans[i]=xor;
+            ans[i]=prefixsum[right+1]^prefixsum[left];
         }
         return ans;
     }
