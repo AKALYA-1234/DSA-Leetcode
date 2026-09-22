3b074094d058769a249bbbfd9571d010c0a80644
AKALYA S <akalyajsa9976@gmail.com>
2026-09-22 16:07:57 +0530

    Time: 23 ms (33.02%) | Memory: 52.7 MB (14.74%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -2,16 +2,17 @@ class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
         int n=nums.length;
         HashMap<Integer,Integer> mp=new HashMap<>();
-        int prefixsum=0,count=0;
+        int prefixsum=0;
+        int ans=0;
         mp.put(0,1);
         for(int i=0;i<n;i++){
             prefixsum+=nums[i];
             int required=prefixsum-goal;
             if(mp.containsKey(required)){
-                count+=mp.get(required);
+                ans+=mp.get(required);
             }
             mp.put(prefixsum,mp.getOrDefault(prefixsum,0)+1);
         }
-        return count;
+        return ans;
     }
 }
